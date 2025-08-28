16e73d6beda008c9ac52ebe153ec25cfbf1d6035
kabir-20 <kabirmanchanda20@gmail.com>
2025-08-29 00:26:18 +0530

    Add files via upload

---
+++ ques_8.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[100];  
+    cout << "Enter the elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        int n = -1;
+
+   
+        for (int j = 0; j < i; j++)
+		 {
+            if (arr[i] == arr[j]) 
+			{
+                n = 1;
+                break;
+            }
+        }
+
+        if (n==-1)
+		{
+		
+            count++;
+        }
+    }
+
+    cout << "Total number of distinct elements: " << count << endl;
+
+    return 0;
+}
+
